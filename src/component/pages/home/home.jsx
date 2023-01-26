044c49884c922a032c0f053bb6335671f0bd4ab2
Vivy <Tomitaviorel@yahoo.com>
2023-01-26 11:54:02 +0200

    feat:added sidebar and home with css

--- src/component/pages/home/home.jsx
+++ src/component/pages/home/home.jsx
@@ -1,7 +1,18 @@
+import Header from '../../header/header';
+import Posts from '../../posts/posts';
+import SideBar from '../../sidebar/sidebar';
 import './home.css';
 
 const Home = () => {
-  return <div className='home'></div>;
+  return (
+    <>
+      <Header />
+      <div className='home'>
+        <Posts />
+        <SideBar />
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/component/sidebar/sidebar.jsx
@@ -0,0 +1,44 @@
+import './sidebar.css';
+
+const SideBar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>ABOUT ME</span>
+        <img
+          src='/image/Ai1.jpg'
+          alt='Ai1'
+          // style={{ width: 200, height: 200 }}
+        />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+          perferendis laudantium tenetur corporis excepturi aspernatur quos
+          praesentium fuga esse numquam, suscipit quas, dolore sapiente maiores
+          reprehenderit soluta. Fuga, voluptas dicta!
+        </p>
+        <div className='sidebarItem'>
+          <span className='sidebarTitle'>CATEGORIES</span>
+          <ul className='sidebarList'>
+            <li className='sidebarListItem'>Life</li>
+            <li className='sidebarListItem'>Music</li>
+            <li className='sidebarListItem'>Style</li>
+            <li className='sidebarListItem'>Sport</li>
+            <li className='sidebarListItem'>Cinema</li>
+            <li className='sidebarListItem'>Tech</li>
+          </ul>
+        </div>
+        <div className='sidebarItem'>
+          <span className='sidebarTitle'>FOLLOW US</span>
+          <div className='sidebarSocial'>
+            <i className='sidebarIcon fa-brands fa-square-facebook'></i>
+            <i className='sidebarIcon fa-brands fa-square-twitter'></i>
+            <i className='sidebarIcon fa-brands fa-square-pinterest'></i>
+            <i className='sidebarIcon fa-brands fa-square-instagram'></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
