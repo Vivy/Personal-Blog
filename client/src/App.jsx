e21d92bbdb67db291ef4a4d31e089fe46dc00f56
Vivy <Tomitaviorel@yahoo.com>
2023-01-27 14:31:10 +0200

    feat:moved everithing to client folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import {
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Router,
+  Routes,
 } from 'react-router-dom';
 import Home from './component/pages/home/home';
 import Login from './component/pages/login/login';
@@ -31,3 +33,15 @@ function App() {
 }
 
 export default App;
+
+// <Router>
+//   <TopBar />
+//   <Routes>
+//     <Route path='/' element={<Home />} />
+//     <Route path='login' element={user ? <Home /> : <Login />} />
+//     <Route path='register' element={user ? <Home /> : <Register />} />
+//     <Route path='settings' element={user ? <Settings /> : <Register />} />
+//     <Route path='write' element={user ? <Write /> : <Register />} />
+//     <Route path='post/:id' element={<Single />} />
+//   </Routes>
+// </Router>
